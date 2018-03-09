e469650266d61210dbfcea5bb4e1f0ee4da7f79b
Sam Gould <sjg326@gmail.com>
2018-03-09 11:24:58 -0800

    connected store to index.js and imported provider to make redux accessible throughout the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import Dashboard from './Dashboard';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Dashboard />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
